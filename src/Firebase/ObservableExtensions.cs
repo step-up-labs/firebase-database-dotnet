ffc81e830e727ac6d877e67bdbb1f903d1e8e1e1
Tomas Bezouska <tomas.bezouska@barclayscapital.com>
2016-07-07 15:21:08 +0200

    AsObservableCollection extension method

---
+++ src/Firebase/ObservableExtensions.cs
@@ -0,0 +1,44 @@
+﻿namespace Firebase.Database
+{
+    using System;
+    using System.Collections.ObjectModel;
+
+    using Firebase.Database.Streaming;
+
+    /// <summary>
+    /// Extensions for <see cref="IObservable{T}"/>.
+    /// </summary>
+    public static class ObservableExtensions
+    {
+        /// <summary>
+        /// Starts observing on given firebase observable and propagates event into an <see cref="ObservableCollection{T}"/>.
+        /// </summary>
+        /// <param name="observable"> The observable. </param>
+        /// <typeparam name="T"> Type of entity. </typeparam>
+        /// <returns> The <see cref="ObservableCollection{T}"/>. </returns> 
+        public static ObservableCollection<T> AsObservableCollection<T>(this IObservable<FirebaseEvent<T>> observable)
+        {
+            var collection = new ObservableCollection<T>();
+
+            observable.Subscribe(f =>
+            {
+                if (f.EventType == FirebaseEventType.InsertOrUpdate)
+                {
+                    var i = collection.IndexOf(f.Object);
+                    if (i >= 0)
+                    {
+                        collection.RemoveAt(i);
+                    }
+
+                    collection.Add(f.Object);
+                }
+                else
+                {
+                    collection.Remove(f.Object);
+                }
+            });
+
+            return collection;
+        }
+    }
+}
