e7e628af78fd9f1655762911890766d048a3bf46
Tomas Bezouska <bezysoftware@outlook.com>
2021-02-27 10:30:03 +0100

    Fix SetHandler disposing query too soon

--- src/Firebase/Offline/SetHandler.cs
+++ src/Firebase/Offline/SetHandler.cs
@@ -6,18 +6,18 @@
 
     public class SetHandler<T> : ISetHandler<T>
     {
-        public virtual Task SetAsync(ChildQuery query, string key, OfflineEntry entry)
+        public virtual async Task SetAsync(ChildQuery query, string key, OfflineEntry entry)
         {
             using (var child = query.Child(key))
             {
                 if (entry.SyncOptions == SyncOptions.Put)
                 {
-                    return child.PutAsync(entry.Data);
+                    await child.PutAsync(entry.Data);
                 }
                 else
                 {
                     
-                    return child.PatchAsync(entry.Data);
+                    await child.PatchAsync(entry.Data);
                 }
             }
         }
